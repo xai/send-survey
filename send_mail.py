f7eaa37a4cf33a2ac3c2d8f9a946ce9762e919c9
Olaf Lessenich <lessenic@fim.uni-passau.de>
2016-02-21 19:23:59 +0100

    Fix formatting and PEP8 warnings.
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- send_mail.py
+++ send_mail.py
@@ -11,8 +11,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -30,13 +30,35 @@ from smtplib import SMTP
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--mailserver", help="set smtp server", type=str, required=True)
-    parser.add_argument("-u", "--user", help="set smtp user", type=str, required=True)
-    parser.add_argument("-p", "--password", help="set smtp password", type=str, required=True)
-    parser.add_argument("-f", "--sender", help="set From address", type=str, required=True)
-    parser.add_argument("-s", "--subject", help="set subject", type=str, required=True)
-    parser.add_argument("-b", "--bcc", help="set Bcc address", type=str)
-    parser.add_argument("recp", default=[], nargs="+", type=str)
+    parser.add_argument("-m", "--mailserver",
+                        help="set smtp server",
+                        type=str,
+                        required=True)
+    parser.add_argument("-u", "--user",
+                        help="set smtp user",
+                        type=str, required=True)
+    parser.add_argument("-p", "--password",
+                        help="set smtp password",
+                        type=str,
+                        required=True)
+    parser.add_argument("-f",
+                        "--sender",
+                        help="set From address",
+                        type=str,
+                        required=True)
+    parser.add_argument("-s",
+                        "--subject",
+                        help="set subject",
+                        type=str,
+                        required=True)
+    parser.add_argument("-b",
+                        "--bcc",
+                        help="set Bcc address",
+                        type=str)
+    parser.add_argument("recp",
+                        default=[],
+                        nargs="+",
+                        type=str)
     args = parser.parse_args()
 
     message_text = []
@@ -68,4 +90,3 @@ if __name__ == "__main__":
 
     smtp.sendmail(sender, all_recps, msg.as_string())
     smtp.quit()
-
